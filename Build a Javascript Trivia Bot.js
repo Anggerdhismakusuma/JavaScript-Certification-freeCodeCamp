7f3c0525afccbca9e66864b53d94aefcf641ce87
Anggerdhismakusuma <kangger97@gmail.com>
2026-01-13 13:40:10 +0700

    Commit answer

---
+++ Build a Javascript Trivia Bot.js
@@ -0,0 +1,5 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName, botLocation, favoriteLanguage;
+
+console.log("My name is " + botName + " and I live in " + botLocation + ".")
---
+++ Build a Sentence Maker.js
@@ -0,0 +1,26 @@
+let adjective, noun, verb, place, adjective2, noun2;
+
+adjective = "quick";
+noun = "fox";
+verb = "jumps";
+place = "fence";
+adjective2 = "lazy";
+noun2 = "dog";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+let secondStory;
+adjective = "happy";
+noun = "cat";
+verb = "dances";
+place = "garden";
+adjective2 = "colorful";
+noun2 = "butterfly";
+
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
+
+let  = 'playfull';
